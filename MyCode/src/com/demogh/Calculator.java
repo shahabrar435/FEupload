2c8c9518d7469ae80853e3c9a879278a67f06b23
shahabrar435 <57430094+shahabrar435@users.noreply.github.com>
2019-11-06 16:09:27 +0530

    First Commit
    Uploading All Files

---
+++ MyCode/src/com/demogh/Calculator.java
@@ -0,0 +1,17 @@
+package com.demogh;
+
+public class Calculator {
+	
+	public int add(int num1,int num2)
+	{
+	   int r = num1 + num2;
+	   return r;
+	}
+	
+	public int sub(int num1,int num2)
+	{
+		int r = num1 - num2;
+		return r;
+	}
+
+}
